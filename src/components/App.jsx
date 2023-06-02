178f6c191ce13bdd262c20354c90df286caff5de
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-06-03 00:46:02 +0300

    smallfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import SharedLayot from './SharedLayout/SharedLayout';
 import { lazy } from 'react';
 
